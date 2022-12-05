e855628f1884a42dcc732c6b07b9a953c937acae
Diego Oliveira <Diego.o.guimaraes29@gmail.com>
2022-12-05 00:03:49 -0300

    Add lib bcrypt and service post

--- src/services/createUser.services.ts
+++ src/services/createUser.services.ts
@@ -1,15 +1,21 @@
 import { IuserRequest, IuserResponse } from '../interfaces/user.interfaces';
+import { AppDataSource } from '../data-source';
+import { User } from '../entities/user.entities';
+import {hash} from 'bcrypt'
 
-const createUserService = ({name,date, phone,email,password} :IuserRequest): IuserResponse =>{
-    return {
-        id:'1',
-        name,
-        email,
-        date,
-        phone
-        
+const createUserService = async ({name,date, phone,email,password} :IuserRequest): Promise<User> =>{
+    const userRepository = AppDataSource.getRepository(User)
+    
+    if(!password){
+        throw new Error('Password is missing')
     }
 
-}
+    const hasedPassword = await hash(password,10)
 
+    const user =  userRepository.create({name,date,email,password:hasedPassword,phone})
+
+    await userRepository.save(user)
+
+    return user
+}
 export default createUserService
