278aa6f35c2aac7666e59b51c1b52f73655d0431
Diego Oliveira <Diego.o.guimaraes29@gmail.com>
2022-12-06 16:50:33 -0300

    Create class appError

--- src/controllers/user.controllers.ts
+++ src/controllers/user.controllers.ts
@@ -4,37 +4,21 @@ import createUserService from '../services/createUser.services';
 import updateUserService from '../services/updateUser.services';
 import { instanceToPlain } from 'class-transformer'
 const createUserController = async (req: Request, res: Response) => {
-    try{
+    
         const user: IuserRequest = req.body
         const createdUser = await createUserService(user)
         return res.json(instanceToPlain(createdUser))
 
-    }catch (error) {
-        if(error instanceof Error){
-
-            return res.status(400).json({
-                    message: error.message
-                })
-        }
-
-    }
+  
 }
 const updateUserController = async (req: Request, res: Response) => {
-    try{
+    
         const user: IuserUpdateRequest = req.body
         const id: string = req.params.id
         const updatedUser = await updateUserService(user,id)
         return res.json(instanceToPlain(updatedUser))
 
-    }catch (error) {
-        if(error instanceof Error){
-
-            return res.status(400).json({
-                    message: error.message
-                })
-        }
-
-    }
+    
 }
 
 export { createUserController,updateUserController}
