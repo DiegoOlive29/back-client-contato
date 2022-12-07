cd6e7f22e2e3c6fa75ff53e299fb3fdc3fc367b2
Diego Oliveira <Diego.o.guimaraes29@gmail.com>
2022-12-07 14:23:08 -0300

    feat: view post and view getId

--- src/controllers/contact.contorllers.ts
+++ src/controllers/contact.contorllers.ts
@@ -2,6 +2,7 @@ import { Request,Response } from "express"
 import { AppError } from "../errors/appError"
 import { IcontactRequest } from "../interfaces/contact.interfaces"
 import createContact from "../services/createContact.services"
+import listContact from "../services/listContactUser.services"
 
 const createContactController = async (req: Request, res: Response) =>{
   
@@ -13,4 +14,12 @@ const createContactController = async (req: Request, res: Response) =>{
 
 
 }
-export {createContactController}
+const listContactController = async (req:Request,res:Response)=>{
+    const id = req.user.id
+
+    const list =await listContact(id)
+
+    return res.json(list)
+}
+
+export {createContactController,listContactController}
--- src/routers/contact.routes.ts
+++ src/routers/contact.routes.ts
@@ -1,8 +1,8 @@
 import { Router } from "express";
-import { createContactController } from "../controllers/contact.contorllers";
+import { createContactController, listContactController } from "../controllers/contact.contorllers";
 import tokenAuthMiddleware from "../middlewares/ensureAuth.middleware";
 
 const contactRoutes = Router();
     contactRoutes.post("", tokenAuthMiddleware,createContactController);
-
+    contactRoutes.get("/user", tokenAuthMiddleware,listContactController);
 export default contactRoutes;
